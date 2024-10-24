8fa19e63de33d0a693ae16aff0a7a169e257ba28
yz <ngohyangzeen@gmail.com>
2024-10-24 11:52:19 +0800

    second commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,13 @@
+import random
+import string
+
 for thing in range(1000):
     print("Hello, World!")
+
+
+    def generate_password(length=12):
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for i in range(length))
+        return password
+
+    print(generate_password())
